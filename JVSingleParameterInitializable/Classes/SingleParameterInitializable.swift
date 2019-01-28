c5a747f8aceaf52358b4e7d975e69c46d62d0c12
Jasper <Jasperav@hotmail.com>
2019-01-28 21:24:52 +0100

    commit

--- JVSingleParameterInitializable/Classes/SingleParameterInitializable.swift
+++ JVSingleParameterInitializable/Classes/SingleParameterInitializable.swift
@@ -1,6 +1,6 @@
 /// Omits spagetti initializers when using composite design pattern and you are using lots of objects in one object.
 public protocol SingleParameterInitializable {
-    associatedtype T
+    associatedtype SingleParameterInitializableObject
     
-    init(from: T)
+    init(from: SingleParameterInitializableObject)
 }
